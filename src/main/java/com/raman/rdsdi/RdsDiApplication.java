a6ec9d1845ba7896432f8ed5be296879df086719
singh-deep-raman <raman111092@gmail.com>
2020-09-07 23:52:03 +0530

    inital project created

---
+++ src/main/java/com/raman/rdsdi/RdsDiApplication.java
@@ -0,0 +1,13 @@
+package com.raman.rdsdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdsDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdsDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raman/rdsdi/RdsDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raman.rdsdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdsDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
